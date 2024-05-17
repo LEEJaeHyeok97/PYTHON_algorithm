b643cab09c06297609e96734c99024211e28425d
LeeJaeHyeok97 <hazardous10@naver.com>
2024-05-18 01:19:33 +0900

    5.18

---
+++ 백준 단계별 문제/1439.py
@@ -0,0 +1,42 @@
+
+s = input()
+
+cnt_0 = 0
+cnt_1 = 0
+
+prev = int(s[0])
+
+if prev == 1:
+    cnt_0 = 1
+else:
+    cnt_0 = 0
+
+#0으로 만들 시
+for i in s[1:]:
+    i = int(i)
+    if prev == i or i == 0:
+        prev = i
+        continue
+    else:
+        cnt_0 += 1
+        prev = i
+
+#1로 만들 시
+prev = int(s[0])
+
+if prev == 1:
+    cnt_1 = 0
+else:
+    cnt_1 = 1
+
+for i in s[1:]:
+    i = int(i)
+    if prev == i or i == 1:
+        prev = i
+        continue
+    else:
+        cnt_1 += 1
+        prev = i
+
+
+print(min(cnt_0, cnt_1))
